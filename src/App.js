7f37ece45bf352c6942a241acaa60d8cc319558e
Mabraca <mbracamonte@ubiipagos.com>
2020-09-20 11:05:00 -0400

    console.log(dish);

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { NavbarBrand, Navbar } from 'reactstrap';
 import './App.css';
+import {DISHES} from './shared/dishes';
 import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorant Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorant Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    )
+  };
 }
 
 export default App;
